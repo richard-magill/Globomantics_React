6cc912a7411e7e10534f4fd4fbb72647cf27f929
Richard Magilll <74077003+richard-magill@users.noreply.github.com>
2022-05-21 16:22:35 -0700

    Add files via upload

---
+++ Globomantics/src/main-page/header.js
@@ -0,0 +1,28 @@
+import logo from "./GloboLogo.png";
+import {Component} from "react";
+
+/* const Header = ({subtitle}) =>  (
+    <header className= 'row'>
+        <div className="col-md-5">
+            <img src={logo} className= "logo" alt="logo" />   
+        </div>
+        <div className="col-md-7 mt-5 subtitle">
+            {subtitle}
+        </div>
+    </header>
+); */
+
+class Header extends Component{
+    render(){
+        return (
+            <header className = "row">
+                <div className = "col-md-5">
+                    <img src= {logo} className = "logo" alt="logo" />
+                </div>
+                <div className= "col-md-7 mt-5 subtitle">{this.props.subtitle}</div>
+            </header>
+        )
+    }
+}
+
+export default Header;
